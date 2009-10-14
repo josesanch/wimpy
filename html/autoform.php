ac286874f583e5620893cd5fd32d4d05db054ac0
José Sánchez Moreno <jose@o2w.es>
2009-10-14 17:54:09 +0200

    Solucionado el tema de las comillas

--- html/autoform.php
+++ html/autoform.php
@@ -1,89 +1,106 @@
-<?
-class html_autoform extends html_form {
+<?php
+class html_autoform extends html_form
+{
     public $buttons = array("back", "delete", "save");
-	public function __construct($model = null, $css = null) {
-		$this->css = $css;
 
-		if(is_object($model)) {
-			$this->setModel($model);
-			parent::__construct(get_class($model), "/admin/".get_class($model)."/save".web::params());
-		} else  {
-			parent::__construct(get_class($model));
-		}
-		$this->construct_head();
-	}
+    public function __construct($model = null, $css = null)
+    {
+        $this->css = $css;
 
-	private function construct_head() {
-		$this->add("
-				<fieldset class='admin_form ".get_class($this->model)."'>
-				<legend>".$this->model->getTitle()."</legend>
-			");
+        if (is_object($model)) {
+            $this->setModel($model);
+            parent::__construct(
+                get_class($model),
+                "/admin/".get_class($model)."/save".web::params()
+            );
+        } else {
+            parent::__construct(get_class($model));
+        }
+        $this->construct_head();
+    }
 
-		if(!$this->model->id) {
-			$tmp_upload = get_class($this->model)."_".rand();
-		 	$this->hidden("tmp_upload")->value($tmp_upload);
-		}
-		foreach($this->model->getAllFieldsForForm() as $field => $type) {
-			if($type == 'separator' || $type == '---') {
-				$this->add("<h2>$field</h2>");
-				continue;
-			}
+    private function construct_head()
+    {
+        $this->add(
+            "   <fieldset class='admin_form ".get_class($this->model)."'>
+                <legend>".$this->model->getTitle()."</legend>"
+        );
 
-			$attrs = $this->model->getFields($field);
-			if($attrs['primary_key']) {
-				$id =$this->model->$field;
-				$this->hidden($field)->value($this->model->$field);
-			} else {
-				$this->auto($field, null, $tmp_upload);
-				if($this->css) $this->css($this->css);
-			}
-			$this->add("\n");
-		}
+        if (!$this->model->id) {
+            $tmpUpload = get_class($this->model)."_".rand();
+             $this->hidden("tmp_upload")->value($tmpUpload);
+        }
 
-		if($this->model->hasImages()) {
-			$this->add(new html_form_files("", $this->model, $tmp_upload));
-		}
-	}
+        foreach ($this->model->getAllFieldsForForm() as $field => $type) {
+            if ($type == 'separator' || $type == '---') {
+                $this->add("<h2>$field</h2>");
+                continue;
+            }
 
-	private function construct_foot()
-	{
-		$this->add("
-    		<script>
-				function delete_item(id) {
-					if(confirm('Está seguro')) {
-						document.location='/admin/".get_class($this->model)."/delete/' + id + '".web::params()."';
-					}
-				}
-				$('#".get_class($this->model)."').validate();
-			</script>
-			<div class='form-buttons'>");
+            $attrs = $this->model->getFields($field);
 
-		if(in_array("back", $this->buttons)) {
-    		$this->add("<input class='submit boton-volver' id='boton-volver'
-    		            type='button' value=volver
-    		            onclick=\"document.location='/admin/".get_class($this->model)."/list".web::params()."'\">"
-    		          );
-		}
+            if ($attrs['primary_key']) {
+                $id =$this->model->$field;
+                $this->hidden($field)->value($this->model->$field);
+            } else {
+                $this->auto($field, null, $tmpUpload);
+                if ($this->css) $this->css($this->css);
+            }
+            $this->add("\n");
+        }
 
-		if($this->model->id && in_array("delete", $this->buttons)) {
-			$this->add("<input class='submit boton-eliminar' id='boton-eliminar'
-			            type='button' value=eliminar
-			            onclick=\"delete_item('".$this->model->id."');\">"
-			          );
+        if ($this->model->hasImages()) {
+            $this->add(new html_form_files("", $this->model, $tmpUpload));
+        }
+    }
 
-		}
+    private function construct_foot()
+    {
+        $this->add(
+            "<script>
+                function delete_item(id) {
+                    if (confirm('Está seguro')) {
+                        document.location='/admin/".
+            get_class($this->model)."/delete/' + id + '".web::params()."';
+                    }
+                }
+                $('#".get_class($this->model)."').validate();
+            </script>
+            <div class='form-buttons'>"
+        );
 
-		if(in_array("save", $this->buttons)) {
-    		$this->add("<input class='submit boton-guardar' type='submit' id='boton-guardar' value='guardar'/>");
-    	}
+        if (in_array("back", $this->buttons)) {
+            $this->add(
+                "<input class='submit boton-volver' id='boton-volver'
+                type='button' value=volver
+                onclick=\"document.location='/admin/".
+                get_class($this->model)."/list".web::params()."'\">"
+            );
+        }
 
-    	$this->add("</div></fieldset>");
-	}
+        if ($this->model->id && in_array("delete", $this->buttons)) {
+            $this->add(
+                "<input class='submit boton-eliminar' id='boton-eliminar'
+                type='button' value=eliminar
+                onclick=\"delete_item('".$this->model->id."');\">"
+            );
 
-	public function toHtml() {
-		$this->construct_foot();
-		return parent::toHtml();
-	}
+        }
+
+        if (in_array("save", $this->buttons)) {
+            $this->add(
+                "<input class='submit boton-guardar'
+                type='submit' id='boton-guardar' value='guardar'/>"
+            );
+        }
+
+        $this->add("</div></fieldset>");
+    }
+
+    public function toHtml()
+    {
+        $this->construct_foot();
+        return parent::toHtml();
+    }
 
 }
-?>
--- html/form.php
+++ html/form.php
@@ -1,209 +1,233 @@
-<?
-class html_form extends html_object {
+<?php
 
-	public $css = null;
-	private $js = "";
+class html_form extends html_object
+{
 
-	protected $attrs = array(
-		'method' => 'POST'
-	);
-	protected $inputs = array();
-	protected $model = null;
+    public $css = null;
+    private $js = "";
 
-	public function __construct($name, $action = null, $method = null, $attrs = array()) {
-		$this->attrs["name"] = $name;
-		if($method) $this->attrs["method"] = $method;
-		$this->attrs['action'] = isset($action) ? $action : $_SERVER["PHP_SELF"];
-		$this->attrs += $attrs;
-	}
+    protected $attrs = array(
+        'method' => 'POST'
+    );
+    protected $inputs = array();
+    protected $model = null;
 
-	public function toHtml() {
-		if($this->attrs["onsubmit"] == "return form(this);") {
-			kernel::loadJS("form");
-		}
+    public function __construct($name, $action = null, $method = null, $attrs = array())
+    {
+        $this->attrs["name"] = $name;
+        if($method) $this->attrs["method"] = $method;
+        $this->attrs['action'] = isset($action) ? $action : $_SERVER["PHP_SELF"];
+        $this->attrs += $attrs;
+    }
 
-
-		$this->process();
-		return "<form ".$this->getAttributes().">\n".$this->data."</form>\n";
-	}
-
-
-	public function css($url) {
-		$this->css = $url;
-	}
-	public function __call($method, $args) {
-
-		// Class name
-		$input = 'html_form_'.$method;
-		switch(count($args)) {
-			case 1:
-				$input = new $input($args[0]);
-			break;
-			case 2:
-				$input = new $input($args[0], $args[1]);
-			break;
-		}
-
-		$this->inputs[]= $input;
-		return $input;
-	}
-
-	public function add($object) {
-		$this->inputs[]= $object;
-	}
+    public function toHtml()
+    {
+        if($this->attrs["onsubmit"] == "return form(this);") {
+            kernel::loadJS("form");
+        }
 
 
-	protected function process() {
-		foreach($this->inputs as $input) {
-			if(is_a($input, 'html_form_file')) $this->attrs['enctype'] = 'multipart/form-data';
-			$this->data .= $input;
-		}
-		$this->data .= "
+        $this->process();
+        return "<form ".$this->getAttributes().">\n".$this->data."</form>\n";
+    }
+
+
+    public function css($url)
+    {
+        $this->css = $url;
+    }
+
+    public function __call($method, $args)
+    {
+
+        // Class name
+        $input = 'html_form_'.$method;
+        switch (count($args)) {
+            case 1:
+                $input = new $input($args[0]);
+            break;
+            case 2:
+                $input = new $input($args[0], $args[1]);
+            break;
+        }
+
+        $this->inputs[]= $input;
+        return $input;
+    }
+
+    public function add($object)
+    {
+        $this->inputs[]= $object;
+    }
+
+
+    protected function process()
+    {
+        foreach($this->inputs as $input) {
+            if(is_a($input, 'html_form_file')) $this->attrs['enctype'] = 'multipart/form-data';
+            $this->data .= $input;
+        }
+        $this->data .= "
 <script>
-	$(document).ready(function() {
-			$this->js
-	});
+    $(document).ready(function() {
+            $this->js
+    });
 </script>";
-	}
+    }
 
-	public function auto($field, $lang = null, $tmp_upload = null) {
+    public function auto($field, $lang = null, $tmp_upload = null)
+    {
 
-		$attrs = $this->model->getFields($field);
+        $attrs = $this->model->getFields($field);
 
-		if(isset($attrs["belongs_to"])) {
-				$model_name = $attrs["belongs_to"];
-				$model_item = new $model_name;
-				$name = $model_item->getTitleField();
+        if(isset($attrs["belongs_to"])) {
+                $model_name = $attrs["belongs_to"];
+                $model_item = new $model_name;
+                $name = $model_item->getTitleField();
 
-				if(!$attrs['autocomplete']) {
-					$input = new html_form_select($field);
-					$input->add($model_item->select("columns: id as value, $name as text"))->select($this->model->$field);
-				} else {
-					// Autocomplete
-					$input = new html_form_input($field."_autocomplete");
-					$input_hidden = new html_form_hidden($field);
-					$primary_key = array_shift($model_item->getPrimaryKeys());
-					if($this->model->$field) {
-						$model_item->select($this->model->$field);
-						$model_item->select("columns: $primary_key as value, $name as text", "$primary_key='".$this->model->$field."'");
-						$input_hidden->value($model_item->value);
-						$input->value($model_item->text);
-					}
+                if(!$attrs['autocomplete']) {
+                    $input = new html_form_select($field);
+                    $input->add($model_item->select("columns: id as value, $name as text"))->select($this->model->$field);
+                } else {
+                    // Autocomplete
+                    $input = new html_form_input($field."_autocomplete");
+                    $input_hidden = new html_form_hidden($field);
+                    $primary_key = array_shift($model_item->getPrimaryKeys());
+                    if($this->model->$field) {
+                        $model_item->select($this->model->$field);
+                        $model_item->select("columns: $primary_key as value, $name as text", "$primary_key='".$this->model->$field."'");
+                        $input_hidden->value($model_item->value);
+                        $input->value($model_item->text);
+                    }
 
-					$this->addJS("
-								$('#{$field}_autocomplete').autocomplete('/ajax/$model_name/autocomplete')
-											.result(function(event, data, formatted) {
-												if (data)
-													$('#$field').val(data[1]);
-												else
-													$('#$field').val('');
-											}).blur(function(){
-											    $(this).search();
-											});
-					");
-					$this->add($input_hidden);
+                    $this->addJS("
+                                $('#{$field}_autocomplete').autocomplete('/ajax/$model_name/autocomplete')
+                                            .result(function(event, data, formatted) {
+                                                if (data)
+                                                    $('#$field').val(data[1]);
+                                                else
+                                                    $('#$field').val('');
+                                            }).blur(function(){
+                                                $(this).search();
+                                            });
+                    ");
+                    $this->add($input_hidden);
 
-				}
+                }
 
-		} else {
-			switch($attrs['type']) {
-				case 'text':
-					if($attrs['html']) {
-						$input = new html_form_htmleditor($lang ? $field."|".$lang : $field);
-						$input->width('100%')->height(300)->css($this->css);
-					} else {
-						$input = new html_form_textarea($lang ? $field."|".$lang : $field);
-						$input->rows(10)->cols(60);
-					}
+        } else {
+            switch($attrs['type']) {
+                case 'text':
+                    if($attrs['html']) {
+                        $input = new html_form_htmleditor($lang ? $field."|".$lang : $field);
+                        $input->width('100%')->height(300)->css($this->css);
+                    } else {
+                        $input = new html_form_textarea($lang ? $field."|".$lang : $field);
+                        $input->rows(10)->cols(60);
+                    }
 
-					$input->value($this->model->get($field, ($lang ? $lang : l10n::instance()->getDefaultLanguage()), false));
-				break;
+                    $input->value($this->model->get($field, ($lang ? $lang : l10n::instance()->getDefaultLanguage()), false));
+                break;
 
-				case 'date':
-					$input = new html_form_input($field);
-					$this->addJS("$('#$field').datepicker({changeMonth: true, changeYear: true}, $.datepicker.regional['es']);\n");
-					$input->size(10);
-					if($this->model->$field != '0000-00-00' and $this->model->$field != '') $input->value(strftime('%d/%m/%Y', strtotime($this->model->$field)));
+                case 'date':
+                    $input = new html_form_input($field);
+                    $this->addJS("$('#$field').datepicker({changeMonth: true, changeYear: true}, $.datepicker.regional['es']);\n");
+                    $input->size(10);
+                    if($this->model->$field != '0000-00-00' and $this->model->$field != '') $input->value(strftime('%d/%m/%Y', strtotime($this->model->$field)));
 
-				break;
+                break;
 
-				case 'image':
-				case 'file':
-					$input = new html_form_file($field, $this->model);
-				break;
+                case 'image':
+                case 'file':
+                    $input = new html_form_file($field, $this->model);
+                break;
 
-				case 'enum':
-					$input  =  new html_form_select($field);
-					$values = array();
-					foreach($attrs['values'] as $val)  $values[$val] =  $val;
-					$input->add($values);
-					$input->select($this->model->$field);
-				break;
+                case 'enum':
+                    $input  =  new html_form_select($field);
+                    $values = array();
+                    foreach($attrs['values'] as $val)  $values[$val] =  $val;
+                    $input->add($values);
+                    $input->select($this->model->$field);
+                break;
 
-				case 'files':
-					$input = new html_form_files($lang ? $field."|".$lang : $field, $this->model, $tmp_upload);
-					break;
+                case 'files':
+                    $input = new html_form_files($lang ? $field."|".$lang : $field, $this->model, $tmp_upload);
+                    break;
 
-				case 'int':
-					$size = $attrs['size'] ? $attrs['size'] : 11;
+                case 'int':
+                    $size = $attrs['size'] ? $attrs['size'] : 11;
 
-				case 'varchar':
-				default:
-					$input = new html_form_input($lang ? $field."|".$lang : $field);
-					if(!$size) $size = $attrs['size'] ? ($attrs['size'] < 45 ? $attrs['size'] : 45)  : 45;
-					$input->size($size);
-					if($attrs['size']) $input->maxsize($attrs['size']);
-					$input->class($input->class()." ".$attrs['validate']);
-					if($attrs['not null']) $input->class($input->class()." required");
-					$input->value($this->model->get($field, ($lang ? $lang : l10n::instance()->getDefaultLanguage()) , false));
-			}
-		}
-		if($lang)
-			$input->label($attrs['label'] ? $attrs['label']." ($lang)" : $field." ($lang)");
-		else
-			$input->label($attrs['label'] ? $attrs['label'] : $field);
+                case 'varchar':
+                default:
+                    $inputName = $lang ? $field."|".$lang : $field;
+                    $input = new html_form_input($inputName);
 
-		$this->inputs[]= $input;
-		if($attrs['l10n'] && !$lang) {
-			foreach(l10n::instance()->getNotDefaultLanguages() as $lang) $this->auto($field, $lang);
-		}
+                    if(!$size)
+                        $size = $attrs['size'] ? ($attrs['size'] < 45 ? $attrs['size'] : 45) : 45;
 
-		return $input;
-	}
+                    $input->size($size);
+                    if($attrs['size']) $input->maxsize($attrs['size']);
+                    $input->class($input->class()." ".$attrs['validate']);
+                    if($attrs['not null']) $input->class($input->class()." required");
+                    $value = $this->model->get($field, ($lang ? $lang : l10n::instance()->getDefaultLanguage()));
 
-	public function remove($field) {
-		$this->inputs = array_filter($this->inputs, create_function('$input', 'return !(is_a($input, "html_object") && $input->name() == '.$field.');'));
-/*		foreach($this->inputs as $id => $input) {
+                    $input->value(str_replace('"', '&quot;', $value), false);
+            }
+        }
 
-			if(is_a($input, 'html_object') && $input->name() == $field) {
-				unset($this->inputs[]);
-			}
-		}
-		*/
-	}
-	public function &get($field) {
-		foreach($this->inputs as $input) {
-			if(is_a($input, 'html_object') && $input->name() == $field) return $input;
-		}
-	}
-	public function addAfter($field, $data) {
-		$count = 0;
+        if($lang)
+            $input->label($attrs['label'] ? $attrs['label']." ($lang)" : $field." ($lang)");
+        else
+            $input->label($attrs['label'] ? $attrs['label'] : $field);
 
-		foreach($this->inputs as $input) {
-			if(is_a($input, 'html_object') && $input->name() == $field) break;
-			$count++;
-		}
-		array_splice($this->inputs, $count+1, 0, $data);
+        $this->inputs[]= $input;
+        if($attrs['l10n'] && !$lang) {
+            foreach(l10n::instance()->getNotDefaultLanguages() as $lang) $this->auto($field, $lang);
+        }
+
+        return $input;
+    }
+
+    public function remove($field)
+    {
+        $this->inputs = array_filter($this->inputs, create_function('$input', 'return !(is_a($input, "html_object") && $input->name() == '.$field.');'));
+/*        foreach($this->inputs as $id => $input) {
+
+            if(is_a($input, 'html_object') && $input->name() == $field) {
+                unset($this->inputs[]);
+            }
+        }
+        */
+    }
+    public function &get($field)
+    {
+        foreach($this->inputs as $input) {
+            if(is_a($input, 'html_object') && $input->name() == $field)
+                return $input;
+        }
+    }
+    public function addAfter($field, $data)
+    {
+        $count = 0;
+
+        foreach($this->inputs as $input) {
+            if(is_a($input, 'html_object') && $input->name() == $field)
+                break;
+            $count++;
+        }
+        array_splice($this->inputs, $count+1, 0, $data);
 
 
-	}
+    }
 
-	public function setModel($model) {
-		$this->model = $model;
-	}
+    public function setModel($model)
+    {
+        $this->model = $model;
+    }
 
-	public function addJS($str) {
-		$this->js .= $str;
-	}
+    public function addJS($str)
+    {
+        $this->js .= $str;
+    }
 }
 ?>
