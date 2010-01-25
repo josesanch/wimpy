7461572acae21c2f91559a5765096c6a7a059497
José Sánchez Moreno <jose@o2w.es>
2010-01-25 13:39:49 +0100

    Cambios

--- html/form/select.php
+++ html/form/select.php
@@ -2,8 +2,8 @@
 class html_form_select extends html_form_input
 {
 
-    protected $selectedValues;
-    protected $disabledValues;
+    protected $selectedValues = array();
+    protected $disabledValues = array();
     protected $attrs = array
     (
         'type'    => 'select',
@@ -78,19 +78,18 @@ class html_form_select extends html_form_input
 
     private function getOptions()
     {
-        foreach ($this->attrs['options'] as $value => $text) {
-            if ($this->selectedValues) {
-                $values = is_array($this->selectedValues) ?
+		$values = is_array($this->selectedValues) ?
                     $this->selectedValues :
                     array($this->selectedValues);
 
-				$disabledValues = is_array($this->disabledValues) ?
-                    $this->disabledValues :
-                    array($this->disabledValues);
+		$disabledValues = is_array($this->disabledValues) ?
+			$this->disabledValues :
+			array($this->disabledValues);
 
-                $selected = in_array($value, $values) ? " selected" : "";
-                $disabled = in_array($value, $disabledValues) ? " disabled" : "";
-            }
+
+        foreach ($this->attrs['options'] as $value => $text) {
+            $selected = in_array($value, $values) ? " selected" : "";
+            $disabled = in_array($value, $disabledValues) ? " disabled" : "";
             $html .= "
                        <option value=\"$value\"$selected{$disabled}>$text</option>";
         }
