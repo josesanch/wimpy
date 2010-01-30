9ac0ba2fe75c875e1a2c1b1c4c320d311933db30
José Sánchez Moreno <jose@timeline.(none)>
2010-01-30 11:46:07 +0100

    FIX: Type en funciones

--- functions.php
+++ functions.php
@@ -35,9 +35,9 @@ function js($module)
 	// Google hosted scripts
 	switch ($module) {
 		case "jquery":
-			return "<script src=\"http://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.min.js\"></script>";
+			return "<script src=\"http://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.min.js\" type=\"text/javascript\"></script>";
 		case "jquery/ui":
-			return "<script src=\"http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/jquery-ui.min.js\"></script>";
+			return "<script src=\"http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/jquery-ui.min.js\" type=\"text/javascript\"></script>";
 	}
 
 	$file = str_replace("::", "/", $module).".js";
