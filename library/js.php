bd86dcd7c0bdd84ecd6628a1a9cdf70fc476bae6
José Sánchez Moreno <jose@o2w.es>
2010-08-12 10:06:51 +0200

    ads

--- library/js.php
+++ library/js.php
@@ -2,11 +2,13 @@
 
 class js {
 
-	public function fancybox($easing  = false) {
-		return 	js_once("jquery")."\n	".
+	public function fancybox($easing  = false, $css = true) {
+		$str = 	js_once("jquery")."\n	".
 				js_once("jquery/fancybox/jquery.fancybox-1.3.1.pack")."\n	".
-				($easing ? js_once("jquery/fancybox/jquery.easing-1.3.pack") : "").
-				css_once("jquery/fancybox/jquery.fancybox-1.3.1")."\n";
+				($easing ? js_once("jquery/fancybox/jquery.easing-1.3.pack") : "");
+
+		if ($css) $str .=css_once("jquery/fancybox/jquery.fancybox-1.3.1")."\n";
+		return $str;
 	}
 
 	public function validate() {
