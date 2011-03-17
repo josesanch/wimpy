869bad4626ee78a18b6e0f4b8a89f079ec11e548
José Sánchez Moreno <jose@o2w.es>
2011-03-17 13:19:54 +0100

    Pulsar en la foto

--- html/form/file.php
+++ html/form/file.php
@@ -26,7 +26,7 @@ class html_form_file extends html_form_input {
 			$str .= "
 				<span id='file_$field'>
 					<input type='hidden' id='_file_$field' name='_file_$field' value='no-delete'/>
-					<img src='".($this->model->$field->src("90x90", 'INABOX'))."' style='border: 1px solid gray; padding: 1px; margin-bottom: 10px; float: left;'/>
+					<a href='".$this->model->$field->url()."'><img src='".($this->model->$field->src("90x90", 'INABOX'))."' style='border: 1px solid gray; padding: 1px; margin-bottom: 10px; float: left;'/></a>
 					<a href=\"javascript:void(document.getElementById('file_$field').innerHTML = '');\"><img src='/resources/icons/cross.gif' border='0' style='border: 1px solid gray; float: left;'/></a>
 				</span>
 			";
