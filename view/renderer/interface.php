e03c5bcdb86b7d26ce654465f4e7da35896c7ebd
José Sánchez Moreno <jose@o2w.es>
2011-11-03 12:00:51 +0100

    Render

--- view/renderer/interface.php
+++ view/renderer/interface.php
@@ -3,7 +3,7 @@
 interface view_renderer_interface
 {
     public function loadTemplate($template);
-    public function render(Array $data);
+    public function render(Array $data = null);
     public function setDirectory($directory, $cacheDirectory = null);
 
     public function __set($item, $value);
